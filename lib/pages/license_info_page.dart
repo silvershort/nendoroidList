0c5e96c3fcd6a2e79d87dd85f1509316c74b3c2f
silvershort <silvershortdev@gmail.com>
2022-07-11 19:33:07 +0900

    스플래시 화면 추가

--- lib/pages/license_info_page.dart
+++ lib/pages/license_info_page.dart
@@ -29,6 +29,20 @@ class LicenseInfoPage extends StatelessWidget {
             height: 10.0,
           ),
           const Text(
+            "- 폰트정보",
+            style: TextStyle(),
+          ),
+          const SizedBox(
+            height: 5.0,
+          ),
+          const Text(
+            "OneMobile, 마비노기옛체",
+            style: TextStyle(),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          const Text(
             "- 오픈소스 라이센스 고지",
             style: TextStyle(),
           ),
@@ -462,6 +476,27 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
 ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."""),
+          licenseButton(context, "flutter_native_splash: ^2.2.4", """MIT License
+
+Copyright (c) 2019 Mark O'Sullivan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""),
         ],
       ),
     );
